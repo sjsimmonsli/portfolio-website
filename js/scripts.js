2e97931a9755b09411a7a49f49ac1aa886259972
Sudie Simmons <sudiesimmons@gmail.com>
2019-02-19 07:49:01 -0500

    Add jQuery to Index

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/* JS here */
+console.log("Welcome to Sudie's Portfolio Site!");
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
