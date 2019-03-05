05fb17284e0625d9c4e952804aaa14af06e41f26
Sudie Simmons <sudiesimmons@gmail.com>
2019-03-04 20:19:49 -0500

    Add new ID to fix error message

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,7 @@
 console.log("Welcome to Sudie's Portfolio Site!");
 
 $(document).ready(function() {
-  $(".project").on("mouseenter", function() {
-    $(this).find(".project-details").fadeIn();
-    });
-  $(".project").on("mouseleave", function() {
-    $(this).find(".project-details").fadeOut();
+  $(".project-preview").on("click", function() {
+    $(this).siblings(".project-details").slideToggle();
     });
 });
